a31352790d9c1be7641aac672fd37f588518f67f
Mads Svejstrup <msv@iterator-it.dk>
2021-11-01 15:44:45 +0100

    Used translateService to generate permission change log description

--- src/app/components/dataset-changelog/dataset-changelog-item/dataset-changelog-item.component.ts
+++ src/app/components/dataset-changelog/dataset-changelog-item/dataset-changelog-item.component.ts
@@ -1,30 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { AccessMemberType, AccessType, DatasetChangeLog, DatasetChangeType, PermissionChangeType } from 'src/app/shared/api/api';
+import { Component, Input, OnInit } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import {
+  AccessMemberType,
+  AccessType,
+  DatasetChangeLog,
+  DatasetChangeType,
+  PermissionChangeType,
+} from "src/app/shared/api/api";
 
 @Component({
-  selector: 'app-dataset-changelog-item',
-  templateUrl: './dataset-changelog-item.component.html',
-  styleUrls: ['./dataset-changelog-item.component.less']
+  selector: "app-dataset-changelog-item",
+  templateUrl: "./dataset-changelog-item.component.html",
+  styleUrls: ["./dataset-changelog-item.component.less"],
 })
 export class DatasetChangelogItemComponent implements OnInit {
-
   @Input()
   change: DatasetChangeLog;
 
-  constructor(private readonly translateService: TranslateService) { }
+  constructor(private readonly translateService: TranslateService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   datasetChangeDescription() {
     switch (this.change.datasetChangeType) {
       case DatasetChangeType.Insert:
-        return this.translateService.instant('dataSteward.meta.changeLog.insert');
+        return this.translateService.instant(
+          "dataSteward.meta.changeLog.insert"
+        );
       case DatasetChangeType.Update:
-        return this.translateService.instant('dataSteward.meta.changeLog.update');
+        return this.translateService.instant(
+          "dataSteward.meta.changeLog.update"
+        );
       case DatasetChangeType.Delete:
-        return this.translateService.instant('dataSteward.meta.changeLog.delete');
+        return this.translateService.instant(
+          "dataSteward.meta.changeLog.delete"
+        );
       case DatasetChangeType.PermissionChange:
         return this.permissionChangeInfo();
     }
@@ -35,14 +45,22 @@ export class DatasetChangelogItemComponent implements OnInit {
       return "";
     }
 
-    var changeType = this.change.datasetPermissionChange.permissionChangeType == PermissionChangeType.Added ? "Granted" : "Revoked";
-    var changeType = changeType;
-    var accessType = AccessType[this.change.datasetPermissionChange.accessType].toLowerCase();
-    var memberType = AccessMemberType[this.change.datasetPermissionChange.accessMemberType].toLowerCase();
-    var displayName = this.change.datasetPermissionChange.displayName;
-    var toOf = this.change.datasetPermissionChange.permissionChangeType == PermissionChangeType.Added ? "to" : "of";
+    var changeType = this.change.datasetPermissionChange.permissionChangeType == PermissionChangeType.Added
+      ? this.translateService.instant("dataSteward.meta.changeLog.granted")
+      : this.translateService.instant("dataSteward.meta.changeLog.revoked");
     
-    return `${changeType} ${accessType} access ${toOf} ${memberType}: '${displayName}'`;
-  }
+    var accessType = AccessType[this.change.datasetPermissionChange.accessType].toLowerCase();
+    accessType = this.translateService.instant(`dataSteward.meta.changeLog.${accessType}`);
+    
+    var memberType = AccessMemberType[this.change.datasetPermissionChange.accessMemberType].toLowerCase();
+    memberType = this.translateService.instant(`dataSteward.meta.changeLog.${memberType}`);
 
+    var displayName = this.change.datasetPermissionChange.displayName;
+
+    var toOf = this.change.datasetPermissionChange.permissionChangeType == PermissionChangeType.Added
+      ? this.translateService.instant("dataSteward.meta.changeLog.to")
+      : this.translateService.instant("dataSteward.meta.changeLog.of");
+
+    return this.translateService.instant("dataSteward.meta.changeLog.permissionChange", {changeType, accessType, toOf, memberType, displayName})
+  }
 }
